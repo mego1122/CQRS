a567dd6a557f6d253a95a29c43be73e091b82754
Mohamed Abdelmageed <mohamedabdelmageed8@outlook.com>
2021-02-03 12:36:15 +0300

    Add project files.

---
+++ CQRS/Context/IApplicationContext.cs
@@ -0,0 +1,13 @@
+﻿using CQRS.Models;
+using Microsoft.EntityFrameworkCore;
+using System.Threading.Tasks;
+
+namespace CQRS.Context
+{
+    public interface IApplicationContext
+    {
+        DbSet<Product> Products { get; set; }
+
+        Task<int> SaveChanges();
+    }
+}
---
+++ CQRS/Features/ProductFeatures/Query/GetProductByIdQuery.cs
@@ -0,0 +1,32 @@
+﻿using CQRS.Context;
+using CQRS.Models;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace CQRS.Features.ProductFeatures.Query
+{
+    public class GetProductByIdQuery : IRequest<Product>
+    {
+        public int Id { get; set; }
+        public class GetProductByIdQueryHandler : IRequestHandler<GetProductByIdQuery, Product>
+        {
+            private readonly IApplicationContext _context;
+            public GetProductByIdQueryHandler(IApplicationContext context)
+            {
+                _context = context;
+            }
+            public async Task<Product> Handle(GetProductByIdQuery query, CancellationToken cancellationToken)
+            {
+                var product = _context.Products.Where(a => a.Id == query.Id).FirstOrDefault();
+                if (product == null) return null;
+                return product;
+            }
+
+         
+        }
+    }
+}
